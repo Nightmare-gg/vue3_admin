c74f1acbfbf826d9d1459295de0ebddc7793611d
2787725910@qq.com <2787725910@qq.com>
2023-06-16 17:38:01 +0800

    没有格式化的代码

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="box">
+    <h1>App根组件</h1>
+  </div>
+</template>
+
+<script lang="ts">
+const str = "我爱你";
+</script>
+
+<style lang="scss">
+.box {
+  width: 600px;
+  height: 400px;
+  background-color: black;
+  h1 {
+    color: white;
+  }
+}
+</style>
